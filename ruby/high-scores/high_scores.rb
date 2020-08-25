b153f37a7cf4dbd7e381b0277439f7671e52e8a2
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2020-08-25 10:37:39 -0500

    Refactor 'High Scores' based on mentor's feedback

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-class Array
-  def maximum
-    max
-  end
-
-  def last_is_maximum?
-    last == maximum
-  end
-
-  def maximums(count = 1)
-    sort.reverse[0, count]
-  end
-end
-
 class HighScores
   attr_reader :scores
 
@@ -22,18 +8,18 @@ class HighScores
   end
 
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    @scores.maximum
+    scores.max
   end
 
   def personal_top_three
-    @scores.maximums 3
+    scores.max 3
   end
 
   def latest_is_personal_best?
-    @scores.last_is_maximum?
+    latest == personal_best
   end
 end
