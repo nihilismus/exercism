ecf4ce70c5b42c242916164d4fe54809c3707eab
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2020-09-01 19:26:45 -0500

    Refactor 'Word Count' based on mentor's feedback

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,11 +1,8 @@
 (ns word-count
   (:require [clojure.string :as str]))
 
-(defn normalize [string]
-  (-> string
-      (str/lower-case)
-      (str/split #"\W+")))
-
 (defn word-count [string]
-  (let [normalized (normalize string)]
-    (frequencies normalized)))
+  (->> string
+    (str/lower-case)
+    (re-seq #"\w+")
+    (frequencies)))
